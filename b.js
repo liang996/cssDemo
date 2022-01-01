80e22d876e62278705ef0435c35678fb58839bbc
liang <mima0000@liangdeMacBook-Pro.local>
2022-01-01 21:02:24 +0800

    添加携程demo

--- b.js
+++ b.js
@@ -1,8 +1,32 @@
-var arr = [2, 8, 5, 0, 5, 2, 6, 7, 2];
-let bj = []
-for (let i = 0; i < arr.length; i++) {
-  if (bj.indexOf(arr[i]) == -1) {
-    bj.push(arr[i]);
-
-  }
-}
+let v = ['15110252', "15110740"]
+let b = [{
+  houseCode: "15110252",
+  houseName: "0101",
+  ridgepoleCode: "43406969",
+  ridgepoleName: "D1",
+  unitCode: "43407779",
+  unitName: "商铺",
+}, {
+  houseCode: "15110255",
+  houseName: "0101",
+  ridgepoleCode: "43406969",
+  ridgepoleName: "D1",
+  unitCode: "43407779",
+  unitName: "商铺",
+}, {
+  houseCode: "15110740",
+  houseName: "0201",
+  ridgepoleCode: "43406969",
+  ridgepoleName: "D1",
+  unitCode: "43407779",
+  unitName: "商铺",
+}]
+let newarr = []
+v.forEach(item => {
+  b.forEach(item1 => {
+    if (item == item1.houseCode) {
+      newarr.push(item1)
+    }
+  })
+})
+console.log(newarr)
